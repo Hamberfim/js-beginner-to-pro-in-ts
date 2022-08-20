9869b652d98cc265d6773b47ae76e1a815da90c4
Hamberfim <adhamlingithub@gmail.com>
2022-08-20 13:24:21 -0500

    object in objects

--- src/chapt3_objects.ts
+++ src/chapt3_objects.ts
@@ -1,4 +1,4 @@
-// objects in objects - migrate to ts
+// objects in objects - migrate to class or interface in ts
 let company = {
   comapanyName: "Healthy Candy",
   marketActivity: "Food Manufacturing",
@@ -11,3 +11,34 @@ let company = {
   },
   established: 2022,
 };
+
+interface Company {
+  comapanyName: string;
+  marketActivity: string;
+  address: {
+    streetNumber: string;
+    street: string;
+    city: string;
+    state: string;
+    zipcode: string;
+  };
+  established: number;
+}
+
+const healthyCandy: Company = {
+  comapanyName: "Healthy Candy",
+  marketActivity: "Food Manufacturing",
+  address: {
+    streetNumber: "1212",
+    street: "2nd Ave",
+    city: "Boca Raton",
+    state: "FL",
+    zipcode: "33427",
+  },
+  established: 2022,
+};
+
+console.log(`${healthyCandy.comapanyName} ${healthyCandy.marketActivity}, Established ${healthyCandy.established}`);
+console.log(
+  `${healthyCandy.address.streetNumber} ${healthyCandy.address.street}, ${healthyCandy.address.city}, ${healthyCandy.address.state} ${healthyCandy.address.zipcode}`
+);
