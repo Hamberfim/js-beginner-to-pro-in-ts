246da670e7596f209fa07856c2c87e4eb6434624
Hamberfim <adhamlingithub@gmail.com>
2022-08-20 14:50:13 -0500

    object in objects

--- src/chapt3_objects.ts
+++ src/chapt3_objects.ts
@@ -14,7 +14,7 @@ let company = {
 
 interface Company {
   comapanyName: string;
-  marketActivity: string;
+  marketActivity: string[];
   address: {
     streetNumber: string;
     street: string;
@@ -27,7 +27,7 @@ interface Company {
 
 const healthyCandy: Company = {
   comapanyName: "Healthy Candy",
-  marketActivity: "Food Manufacturing",
+  marketActivity: ["Food Manufacturing"],
   address: {
     streetNumber: "1212",
     street: "2nd Ave",
@@ -38,7 +38,43 @@ const healthyCandy: Company = {
   established: 2022,
 };
 
-console.log(`${healthyCandy.comapanyName} ${healthyCandy.marketActivity}, Established ${healthyCandy.established}`);
+console.log(`${healthyCandy.comapanyName} (${healthyCandy.marketActivity}), Established ${healthyCandy.established}`);
 console.log(
   `${healthyCandy.address.streetNumber} ${healthyCandy.address.street}, ${healthyCandy.address.city}, ${healthyCandy.address.state} ${healthyCandy.address.zipcode}`
+);
+
+const sportOutlet: Company = {
+  comapanyName: "BIG A Athletics",
+  marketActivity: ["Gym Equipment", "Sports Gear", "Sports Wear"],
+  address: {
+    streetNumber: "999",
+    street: "Sea Side RD",
+    city: "Boca Raton",
+    state: "FL",
+    zipcode: "33427",
+  },
+  established: 1985,
+};
+
+// concatinate marketActivity into a new string for output
+let newMarketString = (() => {
+  let concatMarketString = "";
+  for (let i = 0; i < sportOutlet.marketActivity.length; i++) {
+    if (i < sportOutlet.marketActivity.length - 1) {
+      concatMarketString += sportOutlet.marketActivity[i] + ", ";
+    } else {
+      concatMarketString += sportOutlet.marketActivity[i];
+    }
+  }
+  return concatMarketString;
+})();
+
+console.log(); // space in output
+
+// console.log(
+//   `${sportOutlet.comapanyName} (${sportOutlet.marketActivity[0]}, ${sportOutlet.marketActivity[1]}, ${sportOutlet.marketActivity[2]}), Established ${sportOutlet.established}`
+// );
+console.log(`${sportOutlet.comapanyName} (${newMarketString}), Established ${sportOutlet.established}`);
+console.log(
+  `${sportOutlet.address.streetNumber} ${sportOutlet.address.street}, ${sportOutlet.address.city}, ${sportOutlet.address.state} ${sportOutlet.address.zipcode}`
 );
