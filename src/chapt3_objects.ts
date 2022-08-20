c62c0d5927dc1ce687d62b468185e07fb08d1921
Hamberfim <adhamlingithub@gmail.com>
2022-08-20 13:14:10 -0500

    object in objects

---
+++ src/chapt3_objects.ts
@@ -0,0 +1,13 @@
+// objects in objects - migrate to ts
+let company = {
+  comapanyName: "Healthy Candy",
+  marketActivity: "Food Manufacturing",
+  address: {
+    streetNumber: "1212",
+    street: "2nd Ave",
+    city: "Boca Raton",
+    state: "Florida",
+    zipcode: "33427",
+  },
+  established: 2022,
+};
