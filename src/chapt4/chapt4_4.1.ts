097911d44e8cb27121837e1d969c6aef3a1b3d4a
Hamberfim <adhamlingithub@gmail.com>
2022-08-21 13:20:41 -0500

    chapt4 ex 4.1

---
+++ src/chapt4/chapt4_4.1.ts
@@ -0,0 +1,17 @@
+// create two variables with a boolean value, log to console
+let isHotOven = true;
+
+// check if value is true and output a message to the console
+if (isHotOven) {
+  console.log("Careful, Oven is hot!");
+} else {
+  console.log("It's O.K., Oven is NOT hot!");
+}
+
+// check if value is not true using the negation operatore '!' and output a message to the console
+isHotOven = false; // change boolean value
+if (!isHotOven) {
+  console.log("The oven broke so it's not hot");
+} else {
+  console.log("Watch Out, Oven is hot!");
+}
