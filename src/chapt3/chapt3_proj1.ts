bed359589033ddd04b27c4f8616fcfb2af805225
Hamberfim <adhamlingithub@gmail.com>
2022-08-21 12:42:12 -0500

    chapt3 projetcs-ProductCat

--- src/chapt3/chapt3_proj1.ts
+++ src/chapt3/chapt3_proj1.ts
@@ -1,4 +1,4 @@
-// use this array
+// Manipulation of an Array use this array
 const theListArr = ["Laurence", "Svekis", true, 35, null, undefined, { test: "one", score: 55 }, ["one", "two"]];
 console.log(theListArr);
 // Manipulate the array using
@@ -15,7 +15,7 @@ theListArr[3] = "hello world";
 theListArr[2] = "MIDDLE";
 //       - add 'LAST' to the last position in the array
 theListArr.push("LAST");
-//       - remove null,undefined, and { test: 'one', score: 55 } with one command/method
+//       - remove null,undefined, and { test: 'one', score: 55 } with one array method
 theListArr.splice(4, 3);
 //       - ouput to console
 console.log(theListArr);
---
+++ src/chapt3/chapt3_proj2.ts
@@ -0,0 +1,53 @@
+// implement data structure(s) for a product cataloge and create queries to retrieve data
+type Product = {
+  name: string;
+  model: string;
+  cost: number;
+  quantity: number;
+};
+
+type Inventory = {
+  products: Product[];
+};
+
+// create an array to hold an inventory of store items
+let inventory: Inventory = {
+  products: [],
+};
+
+// create three items, each have properties of name, model/type of product, cost, quantity
+const pizza = {
+  name: "Tony's Pizza",
+  model: "Frozen",
+  cost: 3.99,
+  quantity: 25,
+};
+
+const tea = {
+  name: "Ginger Tea",
+  model: "individual tea bags",
+  cost: 6.99,
+  quantity: 56,
+};
+
+const pop = {
+  name: "Oli-Pop",
+  model: "Orange Cream",
+  cost: 2.99,
+  quantity: 48,
+};
+
+// add all three object to the main array using an array method  -log to console
+inventory.products.push(pizza);
+inventory.products.push(tea);
+inventory.products.push(pop);
+for (const iterator of inventory.products) {
+  console.log(iterator);
+}
+
+// access elements of the 3rd item and log to console,
+console.log(
+  `We have ${inventory.products[2].quantity} ${inventory.products[2].name} ${
+    inventory.products[2].model
+  } soda at $${inventory.products[2].cost.toFixed(2)} `
+);
