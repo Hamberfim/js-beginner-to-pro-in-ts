b521ab1108fdce71d2d922d75ef1ace6c9f1820d
Hamberfim <adhamlingithub@gmail.com>
2022-08-22 17:01:16 -0500

    chapt5 ex5.2

---
+++ src/chapt5/chapt5_ex5.2.ts
@@ -0,0 +1,11 @@
+// set a starting counter to 0
+let counter: number = 0;
+// create a var 'step' to increase your counter
+const STEP: number = 3.333;
+
+// create a do/while loop printing tbhe counter to the console by the step
+do {
+  console.log(counter);
+  counter += STEP;
+} while (counter <= 100);
+// continue the loop until the counter hit a specfic number
