0a97e0a488f069da7a2b4a286cc21c8ba1b797eb
Hamberfim <adhamlingithub@gmail.com>
2022-08-22 12:07:43 -0500

    chapt4 ex4.3

---
+++ src/chapt4/chapt4_ex4.3.ts
@@ -0,0 +1,7 @@
+// create a boolean value for an ID variable
+let isValidID: boolean = true;
+
+// use a ternary operator to create a message variable checking if the ID is valid to enter a venue
+let msg: string = isValidID ? "You may enter the venue." : "You may not enter the venue.";
+// output to console
+console.log(msg);
