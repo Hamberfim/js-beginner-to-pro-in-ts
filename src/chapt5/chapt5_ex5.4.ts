c49d4bb2256ad3e1abc1a78c5d9c5b69ccb1dce4
Hamberfim <adhamlingithub@gmail.com>
2022-08-25 16:24:47 -0500

    played a bit, chapt5 ex5.4

--- src/chapt5/chapt5_ex5.4.ts
+++ src/chapt5/chapt5_ex5.4.ts
@@ -2,7 +2,7 @@
 let myTable: number[][] = [];
 
 // set some const/final variable for the numer of rows/columns
-const rows: number = 5;
+const rows: number = 10;
 const cols: number = 5;
 
 // set up a counter variable to zero- to set the cell content and count the values within the table
@@ -11,13 +11,13 @@ let counter: number = 0;
 // create a for loop to set the number of interations and each row of the table
 for (let i = 0; i < rows; i++) {
   // within that loop set a new temp array - tempTable to hold each row of data
-  let tempTable = [];
+  let tempTable: number[] = []; // rows
   // nest a second loop to count the columns - these are run within the row
   for (let j = 0; j < cols; j++) {
     // increment the main counter on each iteration of the inner loop, tracking the master count of each of the lines cells
     counter++;
     // push the counter value to the tempTable array
-    tempTable.push(counter);
+    tempTable.push(counter); // columns
   }
   // push the tempTable array to the myTable array
   myTable.push(tempTable);
