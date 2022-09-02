23d93b32ceba60ba8214374dccfa5310a24c1b1d
Hamberfim <adhamlingithub@gmail.com>
2022-09-02 15:57:11 -0500

    chapt5 ex5.7

---
+++ src/chapt5/chapt5_ex5.7.ts
@@ -0,0 +1,29 @@
+// type Monitor = {
+//   make: string;
+//   model: string;
+//   screenSize: string;
+//   isUHD: boolean;
+// };
+// create a simple object with multiple items
+let monitorDell: any = {
+  // any required because of the use of a 'for in'
+  make: "Dell",
+  model: "S2340M",
+  screenSize: "23 inches",
+  isUHD: true,
+};
+
+// use a for in loop to get the property names and values outputing to the console
+for (const property in monitorDell) {
+  console.log(`${property}: ${monitorDell[property]}`);
+}
+
+console.log();
+// create an array containing the same items using either a for loop or a for in loop and ouput to the console
+let monitorArray: string[] = [];
+for (const property in monitorDell) {
+  monitorArray.push(monitorDell[property]);
+}
+for (let index = 0; index < monitorArray.length; index++) {
+  console.log(monitorArray[index]);
+}
