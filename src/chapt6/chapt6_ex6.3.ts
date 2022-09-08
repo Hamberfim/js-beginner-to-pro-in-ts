b9153c6a93d7c604855e6ed92e20b6555fbd03a8
Hamberfim <adhamlingithub@gmail.com>
2022-09-08 18:18:05 -0500

    working chapt 6

--- src/chapt6/chapt6_ex6.3.ts
+++ src/chapt6/chapt6_ex6.3.ts
@@ -14,3 +14,32 @@ function determinCalcOperation(num1: number, num2: number, negOperator: string):
 determinCalcOperation(num1, num2, negOperator);
 determinCalcOperation(10, 10, "+");
 determinCalcOperation(22, 42, "+");
+
+// as an arrow function
+let determineCalcOp = (num1: number, num2: number, negOperator: string): void => {
+  if (negOperator === "-") {
+    console.log(`${num1} - ${num2} = ${num1 - num2}`);
+  } else {
+    console.log(`${num1} + ${num2} = ${num1 + num2}`);
+  }
+};
+
+determineCalcOp(2, 4, "+");
+determineCalcOp(4, 3, "-");
+
+const arr: string[] = ["pIZZA", "cAKE", "mILK", "bUTTER", "bREAD"];
+arr.forEach((element) => console.log(element.charAt(0).toUpperCase() + element.substring(1).toLowerCase()));
+
+// spread operator
+let newArr: string[] = [...arr, "iCE CREAM", "sODA", "tOOTHPASTE"];
+console.log(newArr);
+
+function addNums(...args: number[]): void {
+  let total = 0;
+  for (const arg of args) {
+    total += arg;
+  }
+  console.log(total);
+}
+let numArr = [6, 7, 9, 11, 5];
+addNums(...numArr);
