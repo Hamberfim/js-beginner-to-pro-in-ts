cc4a50dbef3b8f21b328a1aa8ec52d7f5aba4312
Hamberfim <adhamlingithub@gmail.com>
2022-10-09 10:11:39 -0500

    lotto gen migrated from C#

---
+++ src/misc_migration/LottoGen.ts
@@ -0,0 +1,41 @@
+/*  Time to play the lottery, without spending a dime! You can write your own random lottery numbers. You can give a try before looking at the solution!
+ *  Write and call the method -
+ *  PowerBall()
+ *      generates 5 random numbers between 1 and 64
+ *      generates a random PowerBall between 5 and 64
+ *      stores numbers in an array with the PowerBall at index zero
+ *      returns the array
+ *  Display the powerball at the end when displaying the numbers in Main(). */
+
+import { stdin, stdout } from "node:process";
+// function to generate random numbers
+function PowerBall() {
+  const MIN: number = 1;
+  const PB_MIN: number = 5;
+  const MAX: number = 65;
+  const MAX_NUMBERS: number = 6;
+
+  let randNumber = Math.floor(Math.random() * (MAX - MIN + 1));
+
+  let fiveRandPlusPowerBall: number[] = [];
+  for (let i = 0; i < MAX_NUMBERS; i++) {
+    let randNumber = Math.floor(Math.random() * (MAX - MIN + 1));
+    fiveRandPlusPowerBall.push(randNumber);
+    // process.stdout.write(` ${randNumber} `);
+  }
+  return fiveRandPlusPowerBall;
+}
+
+let lottoNumber: number[] = PowerBall();
+console.log("\n======== Your Lotto Numbers ========");
+// loop thru all but last number, last number is powerball
+for (let i = 0; i < lottoNumber.length; i++) {
+  if (i < lottoNumber.length - 1) {
+    process.stdout.write(`  ${lottoNumber[i]}`);
+  } else {
+    process.stdout.write(`  POWERBALL: ${lottoNumber[i]}`);
+  }
+}
+console.log("\n======== Your Lotto Numbers ========");
+
+export {};
